5dbe8021c2bbc2854b9b0e22c38f6dfbccfdfc7f
Matthew Gerszewski <dakotaseia@hotmail.com>
2022-02-09 23:07:41 -0600

    1. Added User struct. 2. Added userRoutes group. 3. Added CreateUser method and route.

--- main.go
+++ main.go
@@ -4,21 +4,50 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
+
+type User struct {
+	ID   string `json: "id"`
+	Name string `json: "name"`
+	Age  int    `json: "age"`
+}
+
+var Users []User
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/", getHandler)
+	userRoutes := r.Group("/users")
+	{
+		userRoutes.GET("/", GetUsers)
+		userRoutes.POST("/", CreateUser)
+	}
 
 	if err := r.Run(":5000"); err != nil {
 		log.Fatal(err.Error())
 	}
 }
 
-func getHandler(c *gin.Context) {
+func GetUsers(c *gin.Context) {
+	c.JSON(200, Users)
+}
+
+func CreateUser(c *gin.Context) {
+	var reqBody User
+	if err := c.ShouldBindJSON(&reqBody); err != nil {
+		c.JSON(422, gin.H{
+			"error":   true,
+			"message": "invalid request body",
+		})
+		return
+	}
+
+	reqBody.ID = uuid.New().String()
+
+	Users = append(Users, reqBody)
+
 	c.JSON(200, gin.H{
-		"message":  true,
-		"message2": "hello world",
+		"error": false,
 	})
 }
